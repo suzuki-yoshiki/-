a2c2bfca650fb392aed9c981d05ce3eeb8a6f8dd
suzuki-yoshiki <yoshisu19950418@yahoo.co.jp>
2020-03-18 14:23:45 +0000

    login-function終わり

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "新規登録しました。"
       redirect_to @user
     else

16a9056313c7a24eac9017b527b0161a853ac973
suzuki-yoshiki <yoshisu19950418@yahoo.co.jp>
2020-03-19 07:40:23 +0000

    japan-time-format終わり

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module MemoApp
   class Application < Rails::Application
     config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
